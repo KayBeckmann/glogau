c95a1d31934577b47b37134ffd06bcb5b38eb7ab
Kay <info@beckmann-md.de>
2023-12-11 18:51:32 +0100

    first stage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { MainpageComponent } from "./mainpage/mainpage.component";
+import { ImpressumComponent } from "./impressum/impressum.component";
+import { DsgvoComponent } from "./dsgvo/dsgvo.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: MainpageComponent },
+  { path: "impressum", component: ImpressumComponent },
+  { path: "dsgvo", component: DsgvoComponent }
+];
