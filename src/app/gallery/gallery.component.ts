7fdcb369240e60164aede50461a954689e122b7e
Kay <info@beckmann-md.de>
2023-12-13 11:53:52 +0100

    add pictures to gallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-gallery',
+  selector: "app-gallery",
   standalone: true,
-  imports: [],
-  templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+  imports: [CommonModule],
+  templateUrl: "./gallery.component.html",
+  styleUrl: "./gallery.component.scss"
 })
 export class GalleryComponent {
-
+  path: string = "../../assets/img/";
+  gallery: any = [
+    { name: this.path + "1.jpeg" },
+    { name: this.path + "2.jpeg" },
+    { name: this.path + "3.jpeg" },
+    { name: this.path + "4.jpeg" },
+    { name: this.path + "5.jpeg" },
+    { name: this.path + "6.jpeg" },
+    { name: this.path + "7.jpeg" },
+    { name: this.path + "8.jpeg" },
+    { name: this.path + "9.jpeg" }
+  ];
 }
